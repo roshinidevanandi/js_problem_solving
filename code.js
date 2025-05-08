deb01761fd31c311525625d97af1e239ba967f3a
roshinidevanandi <roshinidevanandi@gmail.com>
2025-05-08 22:49:27 +0530

    yes

--- code.js
+++ code.js
@@ -1,3 +1,5 @@
+// example 1:
+
 var user="roshni";
 var pswd="Kanna@123";
 
@@ -12,4 +14,27 @@ else if(user!="roshni" && pswd=="Kanna@123"){
 }
 else{
     console.log("invalid credentialss")
+}
+
+
+// example 2:
+
+marks=prompt("enter the valid marks: ");
+if(marks>=80){
+    console.log("A grade!");
+}
+else if(marks>65 && marks<=79){
+    console.log("B grade!");
+}
+else if(marks>50 && marks<=64){
+    console.log("C grade!");
+}
+else if(marks>35 && marks<=49){
+    console.log("D grade!");
+}
+else if(marks<35){
+    console.log("Fail!");
+}
+else{
+    console.log("Marks should be greater than zero!")
 }
