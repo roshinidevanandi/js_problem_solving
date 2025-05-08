701b029992f48d34b95a51297b428353f0db4847
roshinidevanandi <roshinidevanandi@gmail.com>
2025-05-08 23:28:50 +0530

    yes

--- code.js
+++ code.js
@@ -63,3 +63,12 @@ for(var i=1;i<=100;i++){
     }
 }
 console.log(`They are ${count} multiples`)
+
+
+// example 5:
+// display tables form 1 to 10
+for(var i=1;i<=10;i++){
+    for(var j=1;j<=10;j++){
+        console.log(`${i}x${j}=${i*j}`)
+    }
+}
