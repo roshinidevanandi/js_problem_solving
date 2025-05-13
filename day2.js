0b8cfa221f7dc349f06439d1a288c4668074a1af
roshinidevanandi <roshinidevanandi@gmail.com>
2025-05-13 20:23:22 +0530

    yes

--- day2.js
+++ day2.js
@@ -8,4 +8,18 @@ while(num>0){
 }
 console.log(len)
 
-// 
+// Find the reverse of a number
+
+num=123
+reverse=0
+while(num>0){
+    digit=num%10
+    reverse=reverse*10+digit
+    num=Math.floor(num/10)
+}
+console.log(reverse)
+
+// find the even and odd numbers count of a given number
+
+
+num=9989017822
